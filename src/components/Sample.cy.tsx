69652c4e5e4b5afae3fbe0335ba349365fdbd550
Piyush Aggarwal <web.piyushaggarwal@gmail.com>
2023-09-05 16:24:40 +0530

    Create Sample.cy.tsx

---
+++ src/components/Sample.cy.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Sample } from './Sample'
+
+describe('<Sample />', () => {
+  it('renders', () => {
+    // see: https://on.cypress.io/mounting-react
+    cy.mount(<Sample />)
+  })
+})
